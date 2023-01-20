fcc1447454b807f239bd4d0aaa82c85a565c8786
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-01-20 20:23:56 +0000

    update scenario#2 test case to run on lambdatest

--- tests/scenario_2.spec.js
+++ tests/scenario_2.spec.js
@@ -1,7 +1,29 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect, chromium } = require('@playwright/test');
 const { Playground } = require('../pages/playground.page');
 
-test('Go to LambdaTest’s, move slider and validate its value', async ({ page }) => {
+test('Go to LambdaTest’s, move slider and validate its value', async () => {
+
+    const capabilities = {
+        'browserName': 'MicrosoftEdge',
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'macOS Mojave',
+            'build': 'Playwright Lamda Test Assesment Build',
+            'name': 'Scenario_2 macOS Mojave - MicrosoftEdge',
+            'user': process.env.LT_USERNAME,
+            'accessKey': process.env.LT_ACCESS_KEY,
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+
+    const page = await browser.newPage();
+
     const playground = new Playground(page);
     await playground.goto();
     await playground.dndSlider.click();
@@ -23,5 +45,15 @@ test('Go to LambdaTest’s, move slider and validate its value', async ({ page }
     const value = await page.locator("#rangeSuccess").textContent();
     console.log("Value",value)
     expect(value).toEqual(targetValue);
+
+    try {
+        expect(value).toEqual(targetValue);
+
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Slider value validated' } })}`)
+    } catch {
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Slider value not validated' } })}`)
+    }
+
+    await browser.close()
 
 })
