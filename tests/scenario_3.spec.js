707e3d934c86fbf61f029b8d86ad19cdaa2f3cff
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-01-20 20:24:42 +0000

    update scenario#3 test case to run on lambdatest

--- tests/scenario_3.spec.js
+++ tests/scenario_3.spec.js
@@ -1,7 +1,29 @@
-const { test, expect } = require('@playwright/test');
+const { chromium, test, expect, webkit, firefox } = require('@playwright/test');
 const { Playground } = require('../pages/playground.page');
 
-test('Go to LambdaTest’s submit empty form and validate error message', async ({ page }) => {
+test('Go to LambdaTest’s submit empty form and validate error message', async () => {
+
+    const capabilities = {
+        'browserName': 'Chrome',
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'Windows 10',
+            'build': 'Playwright Lamda Test Assesment Build',
+            'name': 'Scenario_3 Windows 10 - Chrome',
+            'user': process.env.LT_USERNAME,
+            'accessKey': process.env.LT_ACCESS_KEY,
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+
+    const page = await browser.newPage();
+
     const playground = new Playground(page);
     await playground.goto();
     await playground.inputFormSbmt.click();
@@ -9,18 +31,48 @@ test('Go to LambdaTest’s submit empty form and validate error message', async 
     await playground.sbmtBtn.click();
     await page.waitForTimeout(1000)
 
-    const errorMsg = "Please fill in this field.";
- 
-    const message = await page.evaluate(()=>{
+    const errorMsg = "Please fill out this field.";
+
+    const message = await page.evaluate(() => {
         const name = document.querySelector("#name").validationMessage;
         return name;
     })
 
-    console.log("message", message)
-    expect(message).toEqual(errorMsg)
+    try {
+        expect(message).toEqual(errorMsg)
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Error message found' } })}`)
+    } catch {
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Error message not found' } })}`)
+    }
+
+    await browser.close()
+
 })
 
-test.only('Go to LambdaTest’s submit form and validate success message', async({page})=>{
+test('Go to LambdaTest’s submit form and validate success message', async () => {
+
+    const capabilities = {
+        'browserName': 'pw-firefox',
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'MacOS Monterey',
+            'build': 'Playwright Lamda Test Assesment Build',
+            'name': 'Scenario_3 MacOs Monterey - firefox',
+            'user': process.env.LT_USERNAME,
+            'accessKey': process.env.LT_ACCESS_KEY,
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await firefox.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+
+    const page = await browser.newPage();
+
+
     const playground = new Playground(page);
     await playground.goto();
     await playground.inputFormSbmt.click();
@@ -32,5 +84,15 @@ test.only('Go to LambdaTest’s submit form and validate success message', async
 
     const successMsg = "Thanks for contacting us, we will get back to you shortly.";
     const outputMsg = await page.locator('p.success-msg').textContent();
-    expect(successMsg).toEqual(outputMsg)
+
+
+    try {
+        expect(successMsg).toEqual(outputMsg)
+
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Success message found' } })}`)
+    } catch {
+        await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Succes message not found' } })}`)
+    }
+
+    await browser.close()
 })
